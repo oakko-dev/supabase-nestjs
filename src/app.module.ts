b34fc79ceafb94fc8360321cc5e02ab1c26ce67c
Tanapat Phattharawat <oakko.dev@gmail.com>
2025-11-19 13:05:51 +0700

    Fix PrismaService import path in app.module.ts
    Updated the import path for PrismaService to use a relative path, ensuring correct module resolution.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
-import { PrismaService } from "prisma/prisma.service"
+import { PrismaService } from "../prisma/prisma.service"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
 
